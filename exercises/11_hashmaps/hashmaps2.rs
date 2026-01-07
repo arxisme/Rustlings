fbc39c34db94dd112d6fc7bba4eb6ab58748a765
arxisme <arsh11100arsh@gmail.com>
2026-01-07 20:16:10 +0530

    ah

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let score = basket.get(&fruit).copied().unwrap_or(0);
+        if score==0{
+            basket.insert(fruit,10);
+        }
     }
 }
 
--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -18,6 +18,14 @@ struct TeamScores {
 fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores = HashMap::<&str, TeamScores>::new();
+    let team1goalc:u8;
+    let team2goalc:u8;
+    let team2goals:u8;
+    let team2goals:u8;
+
+
+
+
 
     for line in results.lines() {
         let mut split_iterator = line.split(',');
@@ -26,7 +34,13 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_2_name = split_iterator.next().unwrap();
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
+        let stats1 = scores.entry(team_1_name).or_default();
+        stats1.goals_scored+= team_1_score;
+        stats1.goals_conceded+=team_2_score;
 
+        let stats2 = scores.entry(team_2_name).or_default();
+        stats2.goals_scored+= team_2_score;
+        stats2.goals_conceded+= team_1_score;
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
